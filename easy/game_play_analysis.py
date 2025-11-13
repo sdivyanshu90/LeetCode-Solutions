2b1f27843f49e9e927eefde7466b46bd4edc2c13
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-11-13 22:57:41 +0530

    Game Play Analysis I/1

---
+++ easy/game_play_analysis.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+def game_analysis(activity: pd.DataFrame) -> pd.DataFrame:
+    activity['first_login'] = pd.to_datetime(activity['event_date'])
+    first_login_dates = activity.groupby('player_id')['first_login'].min().reset_index()
+    return first_login_dates
+
+def test_game_analysis():
+    data = {
+        'player_id': [1, 2, 1, 3, 2, 1],
+        'event_date': ['2023-01-01', '2023-01-02', '2023-01-03', '2023-01-01', '2023-01-04', '2023-01-02']
+    }
+    activity_df = pd.DataFrame(data)
+    result = game_analysis(activity_df)
+    print(result)
+
+test_game_analysis()
