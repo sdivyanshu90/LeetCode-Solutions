756cffe9a66e5b6c34b2e6c4d506273e3ac8b08d
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-11-13 23:00:22 +0530

    Game Play Analysis I/1

--- easy/game_play_analysis.py
+++ easy/game_play_analysis.py
@@ -6,6 +6,7 @@ def game_analysis(activity: pd.DataFrame) -> pd.DataFrame:
     return first_login_dates
 
 def test_game_analysis():
+    # Test Case 1
     data = {
         'player_id': [1, 2, 1, 3, 2, 1],
         'event_date': ['2023-01-01', '2023-01-02', '2023-01-03', '2023-01-01', '2023-01-04', '2023-01-02']
