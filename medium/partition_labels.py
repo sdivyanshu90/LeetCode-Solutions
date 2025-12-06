e2a1e0a391870859739d59875b7600316df81297
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-06 12:52:35 +0530

    Partition Labels

--- medium/partition_labels.py
+++ medium/partition_labels.py
@@ -28,6 +28,27 @@ class Solution:
 
         return partition_sizes
 
+# Approach 2
+# class Solution:
+#     def partitionLabels(self, s: str) -> List[int]:
+#         last_pos = defaultdict(int)
+
+#         for i, ch in enumerate(s):
+#             last_pos[ch] = i 
+
+#         start = 0
+#         end = 0
+#         res = []
+
+#         for i, ch in enumerate(s):
+#             end = max(end, last_pos[ch])
+
+#             if i == end:
+#                 res.append(end - start + 1)
+#                 start = i + 1
+
+#         return res
+
 def test_partition_labels():
     solution = Solution()
     
