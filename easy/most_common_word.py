3727f14d6a31ea15612d5d0e689676f1e6e876cc
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-10 12:06:39 +0530

    Most Common Word

---
+++ easy/most_common_word.py
@@ -0,0 +1,38 @@
+from collections import Counter
+import re
+from typing import List
+
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        words = re.findall(r'\w+', paragraph.lower())
+        return Counter([word for word in words if word not in banned]).most_common(1)[0][0]
+
+def test_most_common_word():
+    solution = Solution()
+
+    # Test Case 1
+    paragraph1 = "Bob hit a ball, the hit BALL flew far after it was hit."
+    banned1 = ["hit"]
+    print(solution.mostCommonWord(paragraph1, banned1)) # Expected: "ball"
+
+    # Test Case 2
+    paragraph2 = "a."
+    banned2 = []
+    print(solution.mostCommonWord(paragraph2, banned2)) # Expected: "a"
+
+    # Test Case 3
+    paragraph3 = "Bob. hIt, baLl"
+    banned3 = ["bob", "hit"]
+    print(solution.mostCommonWord(paragraph3, banned3)) # Expected: "ball"
+
+    # Test Case 4
+    paragraph4 = "It was the best of times, it was the worst of times."
+    banned4 = ["it", "was", "the"]
+    print(solution.mostCommonWord(paragraph4, banned4)) # Expected: "times"
+
+    # Test Case 5
+    paragraph5 = "Hello hello HELLO"
+    banned5 = ["allo"]
+    print(solution.mostCommonWord(paragraph5, banned5)) # Expected: ""
+
+test_most_common_word()
