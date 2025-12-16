404aeac2e3126b7c64f88f01769c55f575c14cd9
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-16 12:52:17 +0530

    Smallest Range I/1

---
+++ easy/smallest_range_i.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class Solution:
+    def smallestRangeI(self, nums: List[int], k: int) -> int:
+        return max(0, max(nums) - min(nums) - 2*k)
+
+def test_smallest_range_i():
+    solution = Solution()
+
+    # Test Case 1
+    print(solution.smallestRangeI([1], 0)) # Expected: 0
+
+    # Test Case 2
+    print(solution.smallestRangeI([0,10], 2)) # Expected: 6
+
+    # Test Case 3
+    print(solution.smallestRangeI([1,3,6], 3)) # Expected: 0
+
+    # Test Case 4
+    print(solution.smallestRangeI([5,8,10], 1)) # Expected: 6
+
+    # Test Case 5
+    print(solution.smallestRangeI([2,7,4,1], 3)) # Expected: 0
+
+test_smallest_range_i()
