c2022c9f95397c78746744842d7eea4174636344
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-18 11:56:28 +0530

    Minimum Falling Path Sum

--- medium/min_falling_path_sum.py
+++ medium/min_falling_path_sum.py
@@ -54,7 +54,7 @@ def test_min_falling_path_sum():
 
     # Test case 4
     A4 = [[100,-42,-46,-41],[31,97,10,-10],[-58,-51,82,89],[-51,81,69,-51]]
-    print(solution.minFallingPathSum(A4))  # Expected output: -36
+    print(solution.minFallingPathSum(A4))  # Expected output: -138
 
     # Test case 5
     A5 = [[1,2,3],[4,5,6],[7,8,9]]
