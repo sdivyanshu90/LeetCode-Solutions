8eda5c645afa3aa870256c2eb579c9a07262be68
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-18 12:18:05 +0530

    Reveal Cards In Increasing Order

--- medium/deck_revealed_increasing.py
+++ medium/deck_revealed_increasing.py
@@ -24,19 +24,19 @@ def test_deck_revealed_increasing():
 
     # Test case 1
     deck1 = [17,13,11,2,3,5,7]
-    print(solution.deckRevealedIncreasing(deck1))  # Expected output: [2,13,3,11,5,17,7]
+    print(solution.deckRevealedIncreasing(deck1))  # Expected output: [2, 13, 3, 11, 5, 17, 7]
 
     # Test case 2
     deck2 = [1,1000]
-    print(solution.deckRevealedIncreasing(deck2))  # Expected output: [1,1000]
+    print(solution.deckRevealedIncreasing(deck2))  # Expected output: [1, 1000]
 
     # Test case 3
     deck3 = [3,6,9,1,4,7]
-    print(solution.deckRevealedIncreasing(deck3))  # Expected output: [1,4,7,3,6,9]
+    print(solution.deckRevealedIncreasing(deck3))  # Expected output: [1, 6, 3, 9, 4, 7]
 
     # Test case 4
     deck4 = [10,20,30,40,50]
-    print(solution.deckRevealedIncreasing(deck4))  # Expected output: [10,30,50,20,40]
+    print(solution.deckRevealedIncreasing(deck4))  # Expected output: [10, 50, 20, 40, 30]
 
     # Test case 5
     deck5 = [5]
