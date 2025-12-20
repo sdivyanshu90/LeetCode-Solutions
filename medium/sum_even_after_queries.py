189cc6543d8fb414ad8240a625077b18635a3c2a
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-20 12:38:00 +0530

    Sum of Even Numbers After Queries

--- medium/sum_even_after_queries.py
+++ medium/sum_even_after_queries.py
@@ -21,26 +21,26 @@ def test_sum_even_after_queries():
     # Test case 1
     nums = [1,2,3,4]
     queries = [[1,0],[-3,1],[-4,0],[2,3]]
-    print(solution.sumEvenAfterQueries(nums, queries))  # Expected output: [8,6,2,4]
+    print(solution.sumEvenAfterQueries(nums, queries))  # Expected output: [8, 6, 2, 4]
 
     # Test case 2
     nums = [0]
     queries = [[4,0],[3,0],[-2,0],[1,0]]
-    print(solution.sumEvenAfterQueries(nums, queries))  # Expected output: [4,0,2,2]
+    print(solution.sumEvenAfterQueries(nums, queries))  # Expected output: [4, 0, 0, 6]
 
     # Test case 3
     nums = [2,10,8]
     queries = [[5,1],[-5,0],[5,2],[-10,1]]
-    print(solution.sumEvenAfterQueries(nums, queries))  # Expected output: [10,8,13,3]
+    print(solution.sumEvenAfterQueries(nums, queries))  # Expected output: [10, 8, 0, 0]
 
     # Test case 4
     nums = [1,3,5]
     queries = [[2,1],[4,0],[6,2]]
-    print(solution.sumEvenAfterQueries(nums, queries))  # Expected output: [0,4,10]
+    print(solution.sumEvenAfterQueries(nums, queries))  # Expected output: [0, 0, 0]
 
     # Test case 5
     nums = [-2,0,4]
     queries = [[3,0],[-1,1],[2,2],[-3,0]]
-    print(solution.sumEvenAfterQueries(nums, queries))  # Expected output: [4,4,6,0]
+    print(solution.sumEvenAfterQueries(nums, queries))  # Expected output: [4, 4, 6, 4]
 
 test_sum_even_after_queries()
