76d1cc9eb54b522dcf7c3c0a9e7a626aa23f6cec
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-01 10:59:20 +0530

    Number of Equivalent Domino Pairs

---
+++ easy/num_equiv_domino_pairs.py
@@ -0,0 +1,36 @@
+from typing import List
+
+class Solution:
+    def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
+        num = [0] * 100
+        ret = 0
+        for x, y in dominoes:
+            val = x * 10 + y if x <= y else y * 10 + x
+            ret += num[val]
+            num[val] += 1
+        return ret
+
+def test_num_equiv_domino_pairs():
+    solution = Solution()
+
+    # Test case 1
+    dominoes = [[1,2],[2,1],[3,4],[5,6]]
+    print(solution.numEquivDominoPairs(dominoes))  # Expected output: 1
+
+    # Test case 2
+    dominoes = [[1,2],[1,2],[1,1],[1,2],[2,2]]
+    print(solution.numEquivDominoPairs(dominoes))  # Expected output: 4
+
+    # Test case 3
+    dominoes = [[1,1],[1,1],[1,1]]
+    print(solution.numEquivDominoPairs(dominoes))  # Expected output: 3
+
+    # Test case 4
+    dominoes = [[2,3],[3,2],[2,3],[3,2]]
+    print(solution.numEquivDominoPairs(dominoes))  # Expected output: 6
+
+    # Test case 5
+    dominoes = [[0,0],[0,0],[0,0],[0,0]]
+    print(solution.numEquivDominoPairs(dominoes))  # Expected output: 6
+
+test_num_equiv_domino_pairs()
