49f4e8c9a966df5de7a178ca24269b5ad7534bdc
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-01 11:13:53 +0530

    Article Views I

---
+++ easy/article_views.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+def article_views(views: pd.DataFrame) -> pd.DataFrame:
+    same_author_viewer = views[views['author_id'] == views['viewer_id']]
+    result = same_author_viewer[['author_id']].drop_duplicates().sort_values(by='author_id')
+    result.rename(columns={'author_id': 'id'}, inplace=True)
+    return result
+
+def test_article_views():
+    data = {
+        'author_id': [1, 2, 3, 1, 2, 3, 4],
+        'viewer_id': [1, 2, 4, 3, 2, 3, 4]
+    }
+    views = pd.DataFrame(data)
+    result = article_views(views)
+    print(result)
+
+test_article_views()
