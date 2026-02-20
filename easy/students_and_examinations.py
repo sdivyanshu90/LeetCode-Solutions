53af51e60fd0ec355802684b5b0c9a988b239ff8
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-20 11:32:28 +0530

    Students and Examinations

--- easy/students_and_examinations.py
+++ easy/students_and_examinations.py
@@ -2,11 +2,8 @@ import pandas as pd
 
 def students_and_examinations(students: pd.DataFrame, subjects: pd.DataFrame, examinations: pd.DataFrame) -> pd.DataFrame:
     all_combinations = pd.merge(students, subjects, how='cross')
-
     exam_counts = examinations.groupby(['student_id', 'subject_name']).size().reset_index(name='attended_exams')
-    
     result = pd.merge(all_combinations, exam_counts, how='outer', on=['student_id', 'subject_name']).fillna(0).sort_values(['student_id', 'subject_name'], ascending=True)
-    
     return result[['student_id', 'student_name', 'subject_name', 'attended_exams']]
 
 def test_students_and_examinations():
