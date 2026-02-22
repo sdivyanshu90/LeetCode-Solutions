4d076c5af2b8c5869e5a060d1a8cd8415e627c4b
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-22 11:37:12 +0530

    All Elements in Two Binary Search Trees

--- medium/get_all_elements.py
+++ medium/get_all_elements.py
@@ -1,9 +1,12 @@
+from typing import List, Optional
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+
 class Solution:
     def getAllElements(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> List[int]:
         def inorder_traversal(root, result):
