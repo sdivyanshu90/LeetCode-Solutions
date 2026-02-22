6d9fbbb9643fd644af1c2bc34b72667e8a4b3a48
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-22 11:56:34 +0530

    Minimum Number of Steps to Make Two Strings Anagram

--- medium/min_steps_to_make_two_strings_anagram.py
+++ medium/min_steps_to_make_two_strings_anagram.py
@@ -1,11 +1,9 @@
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
         res = 0
-
         for char in set(t):
             diff = t.count(char) - s.count(char)
             res += diff if diff > 0 else 0
-        
         return res
 
 def test_minSteps():
@@ -14,31 +12,26 @@ def test_minSteps():
     # Test case 1
     s = "bab"
     t = "aba"
-    expected = 1
     print(solution.minSteps(s, t))  # Expected Output: 1
 
     # Test case 2
     s = "leetcode"
     t = "practice"
-    expected = 5
     print(solution.minSteps(s, t))  # Expected Output: 5
 
     # Test case 3
     s = "anagram"
     t = "mangaar"
-    expected = 0
     print(solution.minSteps(s, t))  # Expected Output: 0
 
     # Test case 4
     s = "xxyyzz"
     t = "xxyyzz"
-    expected = 0
     print(solution.minSteps(s, t))  # Expected Output: 0
 
     # Test case 5
     s = "friend"
     t = "family"
-    expected = 4
     print(solution.minSteps(s, t))  # Expected Output: 4
 
 test_minSteps()
