f02cb3e2c299eb143ddf7c2be1bf555107a335f3
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-22 11:54:32 +0530

    Angle Between Hands of a Clock

--- medium/angle_clock.py
+++ medium/angle_clock.py
@@ -14,31 +14,26 @@ def test_angleClock():
     # Test case 1
     hour = 12
     minutes = 30
-    expected = 165.0
     print(solution.angleClock(hour, minutes))  # Expected Output: 165.0
 
     # Test case 2
     hour = 3
     minutes = 30
-    expected = 75.0
     print(solution.angleClock(hour, minutes))  # Expected Output: 75.0
 
     # Test case 3
     hour = 3
     minutes = 15
-    expected = 7.5
     print(solution.angleClock(hour, minutes))  # Expected Output: 7.5
 
     # Test case 4
     hour = 4
     minutes = 50
-    expected = 155.0
     print(solution.angleClock(hour, minutes))  # Expected Output: 155.0
 
     # Test case 5
     hour = 12
     minutes = 0
-    expected = 0.0
     print(solution.angleClock(hour, minutes))  # Expected Output: 0.0
 
 test_angleClock()
