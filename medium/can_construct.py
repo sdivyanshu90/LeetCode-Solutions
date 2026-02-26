e57f7b6930a2047c8bc2f3dacc959c114a41b439
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-26 12:20:29 +0530

    Construct K Palindrome Strings

---
+++ medium/can_construct.py
@@ -0,0 +1,41 @@
+class Solution:
+    def canConstruct(self, s: str, k: int) -> bool:
+        if len(s) < k:
+            return False
+        if len(s) == k:
+            return True
+        odd_count = 0
+
+        for chr in s:
+            odd_count ^= 1 << (ord(chr) - ord("a"))
+        return bin(odd_count).count("1") <= k
+
+def test_can_construct():
+    solution = Solution()
+
+    # Test case 1
+    s1 = "annabelle"
+    k1 = 2
+    print(solution.canConstruct(s1, k1))  # Expected output: True
+
+    # Test case 2
+    s2 = "leetcode"
+    k2 = 3
+    print(solution.canConstruct(s2, k2))  # Expected output: False
+
+    # Test case 3
+    s3 = "true"
+    k3 = 4
+    print(solution.canConstruct(s3, k3))  # Expected output: True
+
+    # Test case 4
+    s4 = "yzyzyzyzyzyzyzy"
+    k4 = 2
+    print(solution.canConstruct(s4, k4))  # Expected output: True
+
+    # Test case 5
+    s5 = "cr"
+    k5 = 7
+    print(solution.canConstruct(s5, k5))  # Expected output: False
+
+test_can_construct()
