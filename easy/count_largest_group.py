eaf966142da67d5400eda2bcf42b9eb8b65f3bc6
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-26 11:47:02 +0530

    Count Largest Group

---
+++ easy/count_largest_group.py
@@ -0,0 +1,34 @@
+class Solution:
+    def countLargestGroup(self, n: int) -> int:
+        hashMap = Counter()
+        for i in range(1, n + 1):
+            key = sum([int(x) for x in str(i)])
+            hashMap[key] += 1
+        maxValue = max(hashMap.values())
+        count = sum(1 for v in hashMap.values() if v == maxValue)
+        return count
+
+def test_count_largest_group():
+    solution = Solution()
+
+    # Test case 1
+    n1 = 13
+    print(solution.countLargestGroup(n1))  # Expected output: 4
+
+    # Test case 2
+    n2 = 2
+    print(solution.countLargestGroup(n2))  # Expected output: 2
+
+    # Test case 3
+    n3 = 15
+    print(solution.countLargestGroup(n3))  # Expected output: 6
+
+    # Test case 4
+    n4 = 24
+    print(solution.countLargestGroup(n4))  # Expected output: 5
+
+    # Test case 5
+    n5 = 1000
+    print(solution.countLargestGroup(n5))  # Expected output: 117
+
+test_count_largest_group()
