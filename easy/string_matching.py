a66c332e1d3ba60fb87141651df10d8234d94b5c
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-26 11:58:49 +0530

    String Matching in an Array

--- easy/string_matching.py
+++ easy/string_matching.py
@@ -1,39 +1,16 @@
 from typing import List
 
 class Solution:
-
-    class TrieNode:
-        def __init__(self):
-            self.frequency = 0
-            self.child_nodes = {}
-
     def stringMatching(self, words: List[str]) -> List[str]:
-        matching_words = []
-        root = self.TrieNode()
+        res = []
+        n = len(words)
+        for i in range(n):
+            for j in range(n):
+                if i != j and words[i] in words[j]:
+                    res.append(words[i])
+                    break
 
-        for word in words:
-            for start_index in range(len(word)):
-                self._insert_word(root, word[start_index:])
-
-        for word in words:
-            if self._is_substring(root, word):
-                matching_words.append(word)
-
-        return matching_words
-
-    def _insert_word(self, root: "TrieNode", word: str) -> None:
-        current_node = root
-        for char in word:
-            if char not in current_node.child_nodes:
-                current_node.child_nodes[char] = self.TrieNode()
-            current_node = current_node.child_nodes[char]
-            current_node.frequency += 1
-
-    def _is_substring(self, root: "TrieNode", word: str) -> bool:
-        current_node = root
-        for char in word:
-            current_node = current_node.child_nodes[char]
-        return current_node.frequency > 1
+        return res
 
 def test_string_matching():
     solution = Solution()
