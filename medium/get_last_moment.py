cf29531a3f8ab201ffb8a9dccb685aa3c9b68db8
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-03-04 12:16:38 +0530

    Last Moment Before All Ants Fall Out of a Plank

--- medium/get_last_moment.py
+++ medium/get_last_moment.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution:
     def getLastMoment(self, n: int, left: List[int], right: List[int]) -> int:
         return max(max(left, default=-1), n - min(right, default=n+1))
