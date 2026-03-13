2627468b5366dfdd22eeedcc09b7baed9fc54f4d
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-03-13 11:15:05 +0530

    Maximum Number of Coins You Can Get

---
+++ medium/max_coins.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxCoins(self, piles: List[int]) -> int:
+        piles.sort(reverse = True)
+        return sum(piles[1:-len(piles)//3:2])
+
+def test_max_coins():
+    solution = Solution()
+
+    # Test case 1
+    piles = [2, 4, 1, 2, 7, 8]
+    print(solution.maxCoins(piles))  # Expected output: 9
+
+    # Test case 2
+    piles = [2, 4, 5]
+    print(solution.maxCoins(piles))  # Expected output: 4
+
+    # Test case 3
+    piles = [9, 8, 7, 6, 5, 1, 2, 3, 4]
+    print(solution.maxCoins(piles))  # Expected output: 18
+
+    # Test case 4
+    piles = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    print(solution.maxCoins(piles))  # Expected output: 30
+
+    # Test case 5
+    piles = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+    print(solution.maxCoins(piles))  # Expected output: 30
+
+test_max_coins()
