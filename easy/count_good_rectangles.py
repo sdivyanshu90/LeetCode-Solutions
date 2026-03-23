bceffa41ec349a0b3efcddaca92e418a03314abd
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-03-23 12:02:14 +0530

    Number Of Rectangles That Can Form The Largest Square

--- easy/count_good_rectangles.py
+++ easy/count_good_rectangles.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution:
     def countGoodRectangles(self, rectangles: List[List[int]]) -> int:
         res = []
@@ -15,18 +17,18 @@ def test_count_good_rectangles():
 
     # Test case 2
     rectangles = [[2, 3], [3, 7], [4, 3], [3, 7]]
-    print(solution.countGoodRectangles(rectangles))  # Expected output: 2
+    print(solution.countGoodRectangles(rectangles))  # Expected output: 3
 
     # Test case 3
     rectangles = [[1, 1], [2, 2], [3, 3], [4, 4]]
-    print(solution.countGood_rectangles(rectangles))  # Expected output: 1
+    print(solution.countGoodRectangles(rectangles))  # Expected output: 1
 
     # Test case 4
     rectangles = [[5, 5], [5, 5], [5, 5], [5, 5]]
-    print(solution.countGood_rectangles(rectangles))  # Expected output: 4
+    print(solution.countGoodRectangles(rectangles))  # Expected output: 4
 
     # Test case 5
     rectangles = [[1, 2], [2, 3], [3, 4], [4, 5]]
-    print(solution.countGood_rectangles(rectangles))  # Expected output: 1
+    print(solution.countGoodRectangles(rectangles))  # Expected output: 1
 
 test_count_good_rectangles()
