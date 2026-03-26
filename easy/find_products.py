c9df6f7a5250066673bb6749713f23207fe69383
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-03-26 16:17:55 +0530

    Recyclable and Low Fat Products

--- easy/find_products.py
+++ easy/find_products.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def find_products(products: pd.DataFrame) -> pd.DataFrame:
     filtered_product = products.loc[(products["low_fats"] == "Y") & (products["recyclable"] == "Y")]
     return filtered_product[['product_id']]
