86b0e0045cec09154fd04aef82fced34eb02d034
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-03-26 16:19:47 +0530

    Minimum Changes To Make Alternating Binary String

---
+++ easy/min_operations_bit.py
@@ -0,0 +1,43 @@
+class Solution:
+    def minOperations(self, s: str) -> int:
+        start0 = 0
+        start1 = 0
+        
+        for i in range(len(s)):
+            if i % 2 == 0:
+                if s[i] == "0":
+                    start1 += 1
+                else:
+                    start0 += 1
+            else:
+                if s[i] == "1":
+                    start1 += 1
+                else:
+                    start0 += 1
+        
+        return min(start0, start1)
+
+def test_min_operations():
+    solution = Solution()
+
+    # Test Case 1
+    s1 = "0100"
+    print(solution.minOperations(s1)) # Expected Output: 1
+
+    # Test Case 2
+    s2 = "10"
+    print(solution.minOperations(s2)) # Expected Output: 0
+
+    # Test Case 3
+    s3 = "1111"
+    print(solution.minOperations(s3)) # Expected Output: 2
+
+    # Test Case 4
+    s4 = "0000"
+    print(solution.minOperations(s4)) # Expected Output: 2
+
+    # Test Case 5
+    s5 = "101010"
+    print(solution.minOperations(s5)) # Expected Output: 0
+
+test_min_operations()
