001e9e9062dea80f086479096fdd643eda5d862c
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-04-12 12:23:16 +0530

    Delete Duplicate Folders in System

--- hard/delete_duplicate_folder.py
+++ hard/delete_duplicate_folder.py
@@ -1,3 +1,6 @@
+from collections import Counter
+from typing import List
+
 class Trie:
     serial: str = ""
     children: dict
@@ -56,22 +59,22 @@ def test_delete_duplicate_folder():
 
     # Test Case 1
     paths1 = [["a"], ["c"], ["d"], ["a", "b"], ["c", "b"], ["d", "a"]]
-    print(solution.deleteDuplicateFolder(paths1))  # Expected output: [["d"], ["d", "a"]]
+    print(solution.deleteDuplicateFolder(paths1))  # Expected output: [['d'], ['d', 'a']]
 
     # Test Case 2
     paths2 = [["a"], ["c"], ["d"], ["a", "b"], ["c", "b"], ["a", "b", "x"], ["a", "b", "x", "y"], ["w"]]
-    print(solution.deleteDuplicateFolder(paths2))  # Expected output: [["c"], ["c", "b"], ["w"]]
+    print(solution.deleteDuplicateFolder(paths2))  # Expected output: [['a'], ['a', 'b'], ['a', 'b', 'x'], ['a', 'b', 'x', 'y'], ['c'], ['c', 'b'], ['d'], ['w']]
 
     # Test Case 3
     paths3 = [["a", "b"], ["c", "d"], ["c"], ["a"]]
-    print(solution.deleteDuplicateFolder(paths3))  # Expected output: [["a"], ["c"]]
+    print(solution.deleteDuplicateFolder(paths3))  # Expected output: [['a'], ['a', 'b'], ['c'], ['c', 'd']]
 
     # Test Case 4
     paths4 = [["a"], ["a", "x"], ["a", "x", "y"], ["a", "z"]]
-    print(solution.deleteDuplicateFolder(paths4))  # Expected output: [["a"], ["a", "z"]]
+    print(solution.deleteDuplicateFolder(paths4))  # Expected output: [['a'], ['a', 'x'], ['a', 'x', 'y'], ['a', 'z']]
 
     # Test Case 5
     paths5 = [["a"], ["b"], ["c"], ["d"]]
-    print(solution.deleteDuplicateFolder(paths5))  # Expected output: [["a"], ["b"], ["c"], ["d"]]
+    print(solution.deleteDuplicateFolder(paths5))  # Expected output: [['a'], ['b'], ['c'], ['d']]
 
 test_delete_duplicate_folder()
