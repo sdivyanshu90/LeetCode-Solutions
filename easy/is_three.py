d83c96296748dffcb05ca9214aa3e1c645929b91
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-04-12 12:12:05 +0530

    Three Divisors

---
+++ easy/is_three.py
@@ -0,0 +1,33 @@
+class Solution:
+    def isThree(self, n: int) -> bool:
+        count = 0
+
+        for i in range(1, n +1):
+            if n % i == 0:
+                count += 1
+        return True if count == 3 else False
+
+def test_is_three():
+    solution = Solution()
+
+    # Test Case 1
+    n1 = 2
+    print(solution.isThree(n1))  # Expected output: False
+
+    # Test Case 2
+    n2 = 4
+    print(solution.isThree(n2))  # Expected output: True
+
+    # Test Case 3
+    n3 = 9
+    print(solution.isThree(n3))  # Expected output: True
+
+    # Test Case 4
+    n4 = 16
+    print(solution.isThree(n4))  # Expected output: False
+
+    # Test Case 5
+    n5 = 25
+    print(solution.isThree(n5))  # Expected output: True
+
+test_is_three()
