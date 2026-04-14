1290be672f10c04a43ceb0b7df5129055975c363
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-04-14 16:43:47 +0530

    Reverse Prefix of Word

---
+++ easy/reverse_prefix.py
@@ -0,0 +1,36 @@
+class Solution:
+    def reversePrefix(self, word: str, ch: str) -> str:
+        idx = word.find(ch)
+        if idx != -1:
+            return word[idx::-1] + word[idx + 1:]
+        return word
+
+def test_reverse_prefix():
+    solution = Solution()
+
+    # Test case 1
+    word1 = "abcdefd"
+    ch1 = "d"
+    print(solution.reversePrefix(word1, ch1))  # Expected output: "dcbaefd"
+
+    # Test case 2
+    word2 = "xyxzxe"
+    ch2 = "z"
+    print(solution.reversePrefix(word2, ch2))  # Expected output: "zxyxxe"
+
+    # Test case 3
+    word3 = "abcd"
+    ch3 = "z"
+    print(solution.reversePrefix(word3, ch3))  # Expected output: "abcd"
+
+    # Test case 4
+    word4 = "a"
+    ch4 = "a"
+    print(solution.reversePrefix(word4, ch4))  # Expected output: "a"
+
+    # Test case 5
+    word5 = "hello"
+    ch5 = "o"
+    print(solution.reversePrefix(word5, ch5))  # Expected output: "olleh"
+
+test_reverse_prefix()
